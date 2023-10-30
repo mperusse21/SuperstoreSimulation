22b0a53aac09d46af7e0e2132d0aad4f08b74ae1
MohammadRahman <rahmanmahbub0529@gmail.com>
2023-10-30 15:22:02 -0400

    Added folder structure for the project

---
+++ Java/superstore/src/main/java/database2project/App.java
@@ -0,0 +1,13 @@
+package database2project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
