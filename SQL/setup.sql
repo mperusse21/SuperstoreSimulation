2653ee48413b4d1cf9f2958a061ee29a9d2f03f6
MohammadRahman <rahmanmahbub0529@gmail.com>
2023-11-01 15:41:28 -0400

    Tested database

--- SQL/setup.sql
+++ SQL/setup.sql
@@ -18,18 +18,18 @@ Address         VARCHAR2(50)
 
 CREATE TABLE Stores (   
 Store_Id        NUMBER(5)          GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-Name            VARCHAR2(30)
+Store_Name      VARCHAR2(30)
 );
 
 CREATE TABLE Products (
 Product_Id      NUMBER(5)          GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-Name            VARCHAR2(30),
+Product_Name    VARCHAR2(30),
 Category        VARCHAR2(15)
 );
 
 CREATE TABLE Warehouse( 
 Warehouse_Id    NUMBER(5)          GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-Name            VARCHAR2(20),
+Warehouse_Name  VARCHAR2(20),
 Address         VARCHAR2(50)
 );
 
@@ -37,12 +37,13 @@ CREATE TABLE Reviews (
 Review_Id       NUMBER(5)          GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 Product_Id      NUMBER(5)          REFERENCES Products(Product_Id),
 Flag            NUMBER(1,0),
-Descrption      VARCHAR2(200)
+Description      VARCHAR2(200)
 );
 
 CREATE TABLE Inventory (
 Warehouse_Id    NUMBER(5)          REFERENCES Warehouse(Warehouse_Id) ,
 Product_Id      NUMBER(5)          REFERENCES Products(Product_Id),
+Stock           NUMBER(10,0),
 
 CONSTRAINT inventory_pk PRIMARY KEY (Warehouse_Id, Product_Id)
 );
@@ -64,4 +65,35 @@ CONSTRAINT ProductsOrders_pk PRIMARY KEY (Order_Id, Product_Id)
 );
 
 COMMIT;
+
+INSERT INTO Customers (Firstname,Lastname,Email,Address) VALUES ('Mahsa','Sadeghi','msadeghi@dawsoncollege.qc.ca','Dawson College, Montreal, Quebec, Canada');
+SELECT * FROM Customers;
+INSERT INTO Stores (Store_Name) VALUES ('Marche Atwater');
+SELECT * FROM Stores;
+INSERT INTO Products (Product_Name,Category) VALUES ('Laptop ASUS 104S','Electronics');
+SELECT * FROM Products;
+INSERT INTO Warehouse (Warehouse_Name,Address) VALUES ('A','100 Rue William, Saint Laurent, Quebec, Canada'); 
+SELECT * FROM Warehouse;
+INSERT INTO Reviews (Product_Id,Flag,Description) VALUES (1,0,'It was affordable');
+SELECT * FROM Reviews;
+INSERT INTO Inventory VALUES(1,1,1000);
+SELECT * FROM Inventory;
+INSERT INTO Orders (Customer_Id,Store_Id,Quantity,OrderDate,Price) VALUES
+(1,1,1,SYSDATE,970);
+SELECT * FROM Orders;
+INSERT INTO Products_Orders VALUES (1,1);
+SELECT * FROM Products_Orders;
+
+SELECT *
+FROM
+Customers INNER JOIN Orders
+USING (customer_id)
+INNER JOIN Products_orders
+USING (order_id)
+INNER JOIN Products
+USING (product_id)
+INNER JOIN Inventory
+USING (product_id)
+INNER JOIN Warehouse
+USING (warehouse_id);
 
