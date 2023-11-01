3111bc75a7f7d1c74f8e24f803cf9b79782f24e0
MohammadRahman <rahmanmahbub0529@gmail.com>
2023-11-01 15:00:53 -0400

    Small changes

--- SQL/setup.sql
+++ SQL/setup.sql
@@ -1,64 +1,67 @@
-DROP TABLE Store CASCADE CONSTRAINTS;
+DROP TABLE Customers CASCADE CONSTRAINTS;
+DROP TABLE Stores CASCADE CONSTRAINTS;
+DROP TABLE Products CASCADE CONSTRAINTS;
+DROP TABLE Warehouse CASCADE CONSTRAINTS;
+DROP TABLE Reviews CASCADE CONSTRAINTS;
+DROP TABLE Inventory CASCADE CONSTRAINTS;
 DROP TABLE Orders CASCADE CONSTRAINTS;
-DROP TABLE PUBLISHER CASCADE CONSTRAINTS;
-DROP TABLE AUTHOR CASCADE CONSTRAINTS;
-DROP TABLE BOOKS CASCADE CONSTRAINTS;
-DROP TABLE ORDERITEMS CASCADE CONSTRAINTS;
-DROP TABLE BOOKAUTHOR CASCADE CONSTRAINTS;
-DROP TABLE PROMOTION CASCADE CONSTRAINTS;
-DROP TABLE ACCTMANAGER CASCADE CONSTRAINTS;
+DROP TABLE Products_Orders CASCADE CONSTRAINTS;
 
 
 CREATE TABLE Customers (
-customer_id CHAR(5) PRIMARY KEY,
-firstname VARCHAR2(20),
-lastname VARCHAR2(20),
-email VARCHAR2(30),
-address VARCHAR2(50)
+Customer_Id     NUMBER(5)          GENERATED ALWAYS AS IDENTITY,
+Firstname       VARCHAR2(20),
+Lastname        VARCHAR2(20),
+Email           VARCHAR2(30),
+Address         VARCHAR2(50)
 );
 
-CREATE TABLE Stores (
-Store_Id CHAR(5) PRIMARY KEY,
-name VARCHAR2(30)
+CREATE TABLE Stores (   
+Store_Id        NUMBER(5)          GENERATED ALWAYS AS IDENTITY,
+Name            VARCHAR2(30)
 );
 
 CREATE TABLE Products (
-Product_Id CHAR(5) PRIMARY KEY,
-name VARCHAR2(30),
-category VARCHAR2(15)
+Product_Id      NUMBER(5)          GENERATED ALWAYS AS IDENTITY,
+Name            VARCHAR2(30),
+Category        VARCHAR2(15)
 );
 
 CREATE TABLE Warehouse( 
-Warehouse_Id CHAR(5) PRIMARY KEY,
-name VARCHAR2(20),
-address VARCHAR2(50)
+Warehouse_Id    NUMBER(5)          GENERATED ALWAYS AS IDENTITY,
+Name            VARCHAR2(20),
+Address         VARCHAR2(50)
 );
 
 CREATE TABLE Reviews (
-review_Id CHAR(5) PRIMARY KEY,
-Product_Id REFERENCES Products(Product_Id),
-flag NUMBER(1,0),
-descrption VARCHAR2(200)
+Review_Id       NUMBER(5)          GENERATED ALWAYS AS IDENTITY,
+Product_Id      NUMBER(5)          REFERENCES Products(Product_Id),
+Flag            NUMBER(1,0),
+Descrption      VARCHAR2(200)
 );
 
 CREATE TABLE Inventory (
-Warehouse_Id CHAR(5) REFERENCES Warehouse(Warehouse_Id) ,
-Product_Id REFERENCES Products(Product_Id),
+Warehouse_Id    NUMBER(5)          REFERENCES Warehouse(Warehouse_Id) ,
+Product_Id      NUMBER(5)          REFERENCES Products(Product_Id),
+
 CONSTRAINT inventory_pk PRIMARY KEY (Warehouse_Id, Product_Id)
 );
 
 CREATE TABLE Orders (
-Order_Id CHAR(5) PRIMARY KEY,
-customer_id CHAR(5) REFERENCES Customers(customer_id),
-Store_Id CHAR(5) REFERENCES Stores(Store_Id),
-quantity NUMBER(2,0),
-orderDate DATE,
-Price NUMBER (10,2)
+Order_Id        NUMBER(5)          GENERATED ALWAYS AS IDENTITY,
+Customer_Id     NUMBER(5)          REFERENCES Customers(customer_id),
+Store_Id        NUMBER(5)          REFERENCES Stores(Store_Id),
+Quantity        NUMBER(2,0),
+OrderDate       DATE,
+Price           NUMBER (10,2)
 );
 
 CREATE TABLE Products_Orders (
-Order_Id CHAR(5) REFERENCES Orders(Order_Id) ,
-Product_Id REFERENCES Products(Product_Id),
+Order_Id        NUMBER(5)          REFERENCES Orders(Order_Id) ,
+Product_Id      NUMBER(5)          REFERENCES Products(Product_Id),
+
 CONSTRAINT ProductsOrders_pk PRIMARY KEY (Order_Id, Product_Id)
 );
+
+COMMIT;
 
