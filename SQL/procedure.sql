02c502d7b544e289481bdd7ad5b1d862323163b8
MohammadRahman <rahmanmahbub0529@gmail.com>
2023-11-13 14:44:24 -0500

    Added packages to 3/5 tables

--- SQL/procedure.sql
+++ SQL/procedure.sql
@@ -1,81 +1,160 @@
 --Cities
 
-CREATE OR REPLACE PROCEDURE add_cities(city_name VARCHAR2, province_name VARCHAR2) IS 
+CREATE OR REPLACE PACKAGE cities_package AS
+
+PROCEDURE add_cities(city_name VARCHAR2, province_name VARCHAR2);
+
+PROCEDURE remove_cities(city_id NUMBER);
+
+PROCEDURE update_cities(city_id NUMBER, city_name VARCHAR2, province_name VARCHAR2);
+
+END cities_package;
+/
+
+CREATE OR REPLACE PACKAGE BODY cities_package AS 
+
+PROCEDURE add_cities(city_name VARCHAR2, province_name VARCHAR2) IS 
 
 BEGIN
 
 INSERT INTO Cities (City, Province) VALUES (city_name, province_name);
 
 END add_cities;
-/
 
-CREATE OR REPLACE PROCEDURE remove_cities(city_id NUMBER) IS
+PROCEDURE remove_cities(city_id NUMBER) IS
 
 BEGIN
 
 DELETE FROM Cities WHERE CityId = city_id;
 
 END remove_cities;
+
+PROCEDURE update_cities(city_id NUMBER, city_name VARCHAR2, province_name VARCHAR2) IS
+
+BEGIN
+
+UPDATE Cities SET City = city_name WHERE CityId = city_id;
+UPDATE Cities SET Province = province_name WHERE CityId = city_id;
+
+END update_cities;
+
+END cities_package;
 /
 
 --Addresses
 
-CREATE OR REPLACE PROCEDURE add_addresses(address_name VARCHAR2, city_id NUMBER) IS
+CREATE OR REPLACE PACKAGE addresses_package AS
+
+PROCEDURE add_addresses(address_name VARCHAR2, city_id NUMBER);
+
+PROCEDURE remove_addresses(address_id NUMBER);
+
+PROCEDURE update_addresses(address_id NUMBER, address_name VARCHAR2);
+
+END addresses_package;
+/
+
+CREATE OR REPLACE PACKAGE BODY addresses_package AS 
+
+PROCEDURE add_addresses(address_name VARCHAR2, city_id NUMBER) IS
 
 BEGIN 
 
 INSERT INTO Addresses (Address, CityId) VALUES (address_name, city_id);
 
 END add_addresses;
-/
 
-CREATE OR REPLACE PROCEDURE remove_addresses(address_id NUMBER) IS
+PROCEDURE remove_addresses(address_id NUMBER) IS
 
 BEGIN
 
 DELETE FROM Addresses WHERE AddressId = address_id;
 
 END remove_addresses;
+
+PROCEDURE update_addresses(address_id NUMBER, address_name VARCHAR2) IS
+
+BEGIN
+
+UPDATE Addresses SET Address = address_name WHERE AddressId = address_id;
+
+END update_addresses;
+
+END addresses_package;
 /
 
 --Stores
 
-CREATE OR REPLACE PROCEDURE add_stores(store_name VARCHAR2) IS
+CREATE OR REPLACE PACKAGE stores_package AS
+
+PROCEDURE add_stores(store_name VARCHAR2);
+
+PROCEDURE remove_stores(store_id NUMBER);
+
+PROCEDURE update_stores(store_id NUMBER, store_name VARCHAR2);
+
+END stores_package;
+/
+
+CREATE OR REPLACE PACKAGE BODY stores_package AS 
+
+PROCEDURE add_stores(store_name VARCHAR2) IS
 
 BEGIN
 
 INSERT INTO Stores (StoreName) VALUES (store_name);
 
 END add_stores;
-/
 
-CREATE OR REPLACE PROCEDURE remove_stores(store_id NUMBER) IS
+PROCEDURE remove_stores(store_id NUMBER) IS
 
 BEGIN
 
 DELETE FROM Stores WHERE StoreId = store_id;
 
 END remove_stores;
+
+PROCEDURE update_stores(store_id NUMBER, store_name VARCHAR2) IS
+
+BEGIN
+
+UPDATE Stores SET StoreName = store_name WHERE StoreId = store_id;
+
+END update_stores;
+
+END stores_package;
 /
 
 --Products
 
-CREATE OR REPLACE PROCEDURE add_products(product_name VARCHAR2, category_name VARCHAR2) IS 
+CREATE OR REPLACE PACKAGE products_package AS
+
+PROCEDURE add_products(product_name VARCHAR2, category_name VARCHAR2);
+
+PROCEDURE remove_products(product_id NUMBER);
+
+END products_package;
+/
+
+CREATE OR REPLACE PACKAGE BODY products_package AS 
+
+PROCEDURE add_products(product_name VARCHAR2, category_name VARCHAR2) IS 
 
 BEGIN
 
 INSERT INTO Products (ProductName, Category) VALUES (product_name, category_name);
 
 END add_products;
-/
 
-CREATE OR REPLACE PROCEDURE remove_products(product_id NUMBER) IS 
+PROCEDURE remove_products(product_id NUMBER) IS 
 
 BEGIN
 
 DELETE FROM Products WHERE ProductId = product_id;
 
 END remove_products;
+
+END products_package;
 /
 
 --Customers
