8ae675c25b6a57082e18505da7d9656a7b56e5b2
MohammadRahman <rahmanmahbub0529@gmail.com>
2023-11-22 20:39:40 -0500

    Arranged some codes

--- Java/superstore/src/main/java/database2project/AuditTable.java
+++ Java/superstore/src/main/java/database2project/AuditTable.java
@@ -14,6 +14,7 @@ public class AuditTable {
     private int changedId;
     private String action;
     private String tableChanged;
+    private Date dateModified;
 
     //Getters for the private fields
     public int getAuditId(){
@@ -30,16 +31,17 @@ public class AuditTable {
     }
 
     //Constructor initializing all private fields
-    public AuditTable(int auditId, int changedId, String action, String tableChanged){
+    public AuditTable(int auditId, int changedId, String action, String tableChanged, Date modifiedDate){
         this.auditId = auditId;
         this.changedId = changedId;
         this.action = action;
         this.tableChanged = tableChanged;
+        this.dateModified = modifiedDate;
     }
 
     @Override
     public String toString(){
-        return "| Audit ID: " + this.auditId + " | ID of changed row: " + this.changedId + " | Action: " + this.action + " | Table Changed: " + this.tableChanged + " |";
+        return "| Audit ID: " + this.auditId + " | ID of changed row: " + this.changedId + " | Action: " + this.action + " | Table Changed: " + this.tableChanged + " | Date Modifed: " + this.dateModified + " |";
     }
 
      public static List<AuditTable> getAuditTable(Connection conn) {
@@ -53,7 +55,7 @@ public class AuditTable {
             stmt.execute();
             results = (ResultSet) stmt.getObject(1);
             while (results.next()){
-                AuditTable allAudit = new AuditTable(results.getInt("AuditId"), results.getInt("ChangedId"), results.getString("Action"), results.getString("TableChanged")); 
+                AuditTable allAudit = new AuditTable(results.getInt("AuditId"), results.getInt("ChangedId"), results.getString("Action"), results.getString("TableChanged"), results.getDate("DateModified")); 
                 auditList.add(allAudit);
             }
         }
--- Java/superstore/src/main/java/database2project/MohTests.java
+++ Java/superstore/src/main/java/database2project/MohTests.java
@@ -6,8 +6,7 @@ import java.util.*;
 public class MohTests {
     public static void main( String[] args ) {
         Scanner reader = new Scanner(System.in);
-        ProductsServices conn = null;
-        CustomersServices connection = null;
+        MohServices connection = null;
 
         try {
             // Prompt user for username and password
@@ -15,10 +14,9 @@ public class MohTests {
             String user = reader.next();
             String password = new String(System.console().readPassword("Password: "));
 
-            conn = new ProductsServices(user, password);
-            connection = new CustomersServices(user, password);
-            //conn.updateProductName(1, "Apple");
-            //conn.updateProductCategory(1, "Grocery");
+            connection = new MohServices(user, password);
+            connection.updateProductName(1, "Apple");
+            connection.updateProductCategory(1, "Groceryy");
             Customers customer = connection.getCustomerByEmail("msadeghi@dawsoncollege.qc.ca");
             System.out.println(customer);
             Customers anotherCustomer = connection.getCustomerById(2);
@@ -27,20 +25,20 @@ public class MohTests {
             System.out.println(address);
             String city = connection.getCity(1);
             System.out.println(city);
-            //conn.addProduct("Broccoli", "Grocery");
-            //Products product = conn.getProductById(14);
-            //System.out.println(product);
-            //List<Products> products = conn.getProductsByCategory("Grocery");
-            //System.out.println(products.get(1));
-            //List<AuditTable> audits = conn.getAuditTable();
-            //for (AuditTable audit : audits) {
-                //System.out.println(audit);
-            //} 
-            List<Products> allProducts = conn.getAllProducts();
+            connection.addProduct("Broccoli", "Grocery");
+            Products product = connection.getProductById(14);
+            System.out.println(product);
+            List<Products> products = connection.getProductsByCategory("Grocery");
+            System.out.println(products.get(1));
+            List<AuditTable> audits = connection.getAuditTable();
+            for (AuditTable audit : audits) {
+                System.out.println(audit);
+            } 
+            List<Products> allProducts = connection.getAllProducts();
             for (Products allProduct : allProducts ){
                 System.out.println(allProduct);
             }
-            String store = conn.getStore(2);
+            String store = connection.getStore(2);
             System.out.println(store);
             List<Customers> allCustomers = connection.getAllCustomers();
             for (Customers allCustomer : allCustomers ){
@@ -56,7 +54,7 @@ public class MohTests {
         // Closes the connection and the Scanner
         finally {
             try {
-                conn.close();
+                connection.close();
             } 
             catch (SQLException e) {
                 e.printStackTrace();
